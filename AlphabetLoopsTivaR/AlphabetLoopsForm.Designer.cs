c0e0f976fdc1bac9224c8c92330e90485f24d9e1
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-06-17 21:32:07 -0400

    Initial commit

---
+++ AlphabetLoopsTivaR/AlphabetLoopsForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace AlphabetLoopsTivaR
+{
+    partial class frmAlphabetLoops
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstLetters = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(148, 41);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstLetters
+            // 
+            this.lstLetters.FormattingEnabled = true;
+            this.lstLetters.Location = new System.Drawing.Point(86, 106);
+            this.lstLetters.Name = "lstLetters";
+            this.lstLetters.Size = new System.Drawing.Size(268, 186);
+            this.lstLetters.TabIndex = 1;
+            // 
+            // frmAlphabetLoops
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 368);
+            this.Controls.Add(this.lstLetters);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmAlphabetLoops";
+            this.Text = "Alphabet Nested Loops By Tiva Rait";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstLetters;
+    }
+}
+
---
+++ AlphabetLoopsTivaR/AlphabetLoopsForm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 17-06-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #25 - Alphabet Nested Loops
+ * This program spits unicode out into the listbox
+*/
+
+namespace AlphabetLoopsTivaR
+{
+    public partial class frmAlphabetLoops : Form
+    {
+        public frmAlphabetLoops()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            string firstLetter;
+            string secondLetter;
+
+            int counterOutside;
+            int counterInside;
+
+            // clear listbox
+            this.lstLetters.Items.Clear();
+
+            // Loop through the letters from A to Z
+            for (counterOutside = 65; counterOutside <= 90; counterOutside++)
+            {
+                firstLetter = Char.ConvertFromUtf32(counterOutside);
+
+                for (counterInside = 65; counterInside <= 90; counterInside++)
+                {
+                    secondLetter = Char.ConvertFromUtf32(counterInside);
+
+                    this.lstLetters.Items.Add(firstLetter + " ->" + secondLetter);
+                }
+            }
+        }
+    }
+}
